13616d552bc994fd746ffe318e71a7039390eb06
talha7-bit <talha7.webdev@gmail.com>
2025-09-27 22:41:47 -0700

    frontend url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(cookieParser())
 dotenv.config()
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://streamfrontendp.vercel.app",
   credentials:true
 }))
 app.use(express.json());
